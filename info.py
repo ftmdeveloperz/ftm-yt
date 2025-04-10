7825555d70788bf7e111396ba1723bce03836996
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-04-10 17:28:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,15 +1,15 @@
 import os
 
-SESSION = "my_bot"
-API_ID = int(os.getenv("API_ID", "8239"))
-API_HASH = os.getenv("API_HASH", "171e6f1bf6d8dcc5140fbe827b6b08")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "807590ZxZZnChpX5srczTxgxz5YmHQ8")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1004232975"))
-DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002232975"))
+SESSION = "ftmbotzx"
+API_ID = int(os.getenv("API_ID", 22141398))
+API_HASH = os.getenv("API_HASH", '0c8f8bd171e05e42d6f6e5a6f4305389')
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8105194942:AAHisU5gxAc77O5qKoAZluI86w10s9MBV5o")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002613994353"))
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002613994353"))
 PORT = int(os.getenv("PORT", "8080"))
-FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "-1002379643238"))
+FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "-1002087228619"))
 HTTP_PROXY = ''
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv:089@cluster0.y8tpouc.mongodb.net/?retryWrites=true&w=majority")
-MONGO_NAME = os.getenv("MONGO_NAME", "YouTubeDL")
-ADMINS = [5660839376, 6167872503]
-DAILY_LIMITS = 20
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ftm:ftm@cluster0.xotfi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_NAME = os.getenv("MONGO_NAME", "Cluster0")
+ADMINS = [7744665378]
+DAILY_LIMITS = 10 
