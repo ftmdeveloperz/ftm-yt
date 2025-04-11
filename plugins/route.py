54081bdf12813146d6e70f11684837ecfaeb3f79
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-04-11 08:11:20 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,7 +1,31 @@
 from aiohttp import web
+import datetime
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("DQTheFileDonor")
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    message = f"""
+🎬✨ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ꜰᴛᴍ ᴛᴜʙᴇꜰᴇᴛᴄʜ ✨🎬
+
+🚀 ʏᴏᴜʀ ᴜʟᴛɪᴍᴀᴛᴇ ᴅᴇꜱᴛɪɴᴀᴛɪᴏɴ ꜰᴏʀ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ʏᴏᴜᴛᴜʙᴇ 🎥 ᴠɪᴅᴇᴏꜱ, ꜱʜᴏʀᴛꜱ & ᴀᴜᴅɪᴏꜱ ɪɴ ꜱᴇᴄᴏɴᴅꜱ!
+💫 ɴᴏ ᴄᴏᴍᴘʟɪᴄᴀᴛɪᴏɴꜱ — ᴊᴜꜱᴛ ꜱᴇɴᴅ ᴀɴʏ ʏᴏᴜᴛᴜʙᴇ ʟɪɴᴋ ᴀɴᴅ ᴄʜᴏᴏꜱᴇ ᴍᴘ3/ᴍᴘ4 ꜰᴏʀᴍᴀᴛ.
+
+🔸 ꜰᴇᴀᴛᴜʀᴇꜱ 🔸
+✅ ꜱᴜᴘᴘᴏʀᴛꜱ ᴠɪᴅᴇᴏꜱ, ꜱʜᴏʀᴛꜱ, ᴍᴘ3, ᴍᴘ4 ɪɴ ʜᴅ ǫᴜᴀʟɪᴛʏ  
+🎵 ᴅᴏᴡɴʟᴏᴀᴅ ᴀᴜᴅɪᴏ (ᴍᴘ3) & ᴠɪᴅᴇᴏ (ᴍᴘ4)  
+🔹 ɴᴏ ᴡᴀᴛᴇʀᴍᴀʀᴋ, ꜰᴜʟʟ ʜᴅ ᴏᴜᴛᴘᴜᴛ  
+🌟 ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟꜱ ꜰᴏʀ ᴇᴀᴄʜ ᴅᴏᴡɴʟᴏᴀᴅ  
+⚡ ꜱᴜᴘᴇʀ ꜰᴀꜱᴛ, ᴀᴅ-ꜰʀᴇᴇ & ꜱɪᴍᴘʟᴇ ᴛᴏ ᴜꜱᴇ
+
+────────────────────────  
+✨ ᴄʀᴇᴀᴛᴇᴅ ᴡɪᴛʜ ʟᴏᴠᴇ ʙʏ: <a href="https://t.me/ftmdeveloperz">ꜰᴛᴍ ᴅᴇᴠᴇʟᴏᴘᴇʀᴢ</a>
+
+<br><br>
+✅ <b>Server Status:</b> Live  
+📡 <b>Status:</b> Success  
+🕒 <b>Time:</b> {now}
+"""
+    return web.Response(text=message, content_type="text/html")
